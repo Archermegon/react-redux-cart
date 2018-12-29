3b0cab05906afcf32861356f867d65ef45213e9a
Archermegon <15076073321@163.com>
2018-12-29 11:10:39 +0800

    11

--- src/actions/index.js
+++ src/actions/index.js
@@ -82,14 +82,10 @@ export const change = id => {
 // };
 export const minus = (proid, num) => {
   return dispatch => {
-    axios
-      .patch(`http://localhost:2019/products/${proid}`, { inventory: num })
-      .then(res => {
-        dispatch({
-          type: actionTypes.MINUS,
-          proid
-        });
-      });
+    dispatch({
+      type: actionTypes.MINUS,
+      proid
+    });
   };
 };
 
